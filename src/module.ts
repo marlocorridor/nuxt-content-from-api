0ad8de82368df77c5ca05d540b93b7fe244ddcb7
Marlo <corridor.marlo@gmail.com>
2024-11-13 14:58:29 +0800

    chore(release): v0.1.1

--- src/module.ts
+++ src/module.ts
@@ -1,13 +1,9 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
-import { existsSync, mkdirSync, writeFileSync } from 'node:fs';
+import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {
-  apiBase: string,
-  apiPath: string,
-  fetchOption: object,
-  directory: string,
-  verbose: boolean
+  apiBase: string, apiPath: string, fetchOption: object, directory: string, verbose: boolean
 }
 
 export default defineNuxtModule<ModuleOptions>({
@@ -27,17 +23,17 @@ export default defineNuxtModule<ModuleOptions>({
     const resolver = createResolver(import.meta.url)
 
     nuxt.hook('build:before', () => {
-      const url = options.apiBase + options.apiPath;
+      const url = options.apiBase + options.apiPath
       fetch(url, options.fetchOption)
           .then(response => response.json())
           .then(jsonData => {
-            console.info('Processing Content from API: Starting...');
+            console.info('Processing Content from API: Starting...')
 
             // Log response if verbose option is true
             if (options.verbose) {
-              console.info('Logging Content from API response...');
-              console.log(jsonData);
-              console.info('End of Logging API response.');
+              console.info('Logging Content from API response...')
+              console.log(jsonData)
+              console.info('End of Logging API response.')
             }
 
             // create directory if not exist
@@ -45,15 +41,15 @@ export default defineNuxtModule<ModuleOptions>({
               mkdirSync(options.directory, {
                 // supports nested directory
                 recursive: true
-              });
+              })
             }
 
             // write the filed
             jsonData.forEach((entry: { slug: string; content: string | NodeJS.ArrayBufferView; }) => {
               // skip if required data is not available
               if (!entry.slug || !entry.content) {
-                console.error('`slug` and/or `content` not found. Skipping...');
-                return;
+                console.error('`slug` and/or `content` not found. Skipping...')
+                return
               }
 
               // write the file using entry.slug as name for routing
@@ -61,10 +57,10 @@ export default defineNuxtModule<ModuleOptions>({
                 options.directory + entry.slug + '.md',
                 entry.content, { flag : 'w'}
               )
-            });
+            })
 
-            console.info('Processing Content from API: Completed!');
-          });
+            console.info('Processing Content from API: Completed!')
+          })
     })
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
