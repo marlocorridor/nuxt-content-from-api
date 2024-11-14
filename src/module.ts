9b65656ea7975d0b8d3f5368ea35d7569ef7caba
Marlo <corridor.marlo@gmail.com>
2024-11-14 15:18:33 +0800

    Enable customizable attribute name

--- src/module.ts
+++ src/module.ts
@@ -6,6 +6,8 @@ export interface ModuleOptions {
   apiBase: string
   apiPath: string
   fetchOption: object
+  slugAttribute: string
+  contentAttribute: string
   directory: string
   verbose: boolean
 }
@@ -20,6 +22,8 @@ export default defineNuxtModule<ModuleOptions>({
     apiBase: 'https://mocki.io/v1', // 'https://YOURHOST/api',
     apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844', // '/blog/all'
     fetchOption: {},
+    slugAttribute: 'slug',
+    contentAttribute: 'content',
     directory: './content/',
     verbose: false,
   },
@@ -48,18 +52,27 @@ export default defineNuxtModule<ModuleOptions>({
             })
           }
 
-          // write the filed
-          jsonData.forEach((entry: { slug: string, content: string | NodeJS.ArrayBufferView }) => {
+          // Define keys for attribute options
+          const slug = options.slugAttribute;
+          const content = options.contentAttribute;
+
+          // Define with variable attribute. See reference: https://dev.to/jagroop2001/how-to-choose-your-type-typescript-interfaces-vs-types-51hc
+          type Entry = {
+            [key: string]: string;
+          };
+
+          // write the file
+          jsonData.forEach((entry: Entry, i: number) => {
             // skip if required data is not available
-            if (!entry.slug || !entry.content) {
-              console.error('`slug` and/or `content` not found. Skipping...')
+            if (!entry[slug] || !entry[content]) {
+              console.error(`Data #${i}: "${slug}" or "${content}" not found. Skipping...`)
               return
             }
 
             // write the file using entry.slug as name for routing
             writeFileSync(
               options.directory + entry.slug + '.md',
-              entry.content, { flag: 'w' },
+              entry[content], { flag: 'w' },
             )
           })
 
