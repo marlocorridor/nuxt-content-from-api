7c931af8a3bf277a9f6422c9ee3f82b948fe93e2
Marlo <corridor.marlo@gmail.com>
2024-11-15 15:40:21 +0800

    Update playground for sample

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,19 +1,33 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: [
+    '../src/module',
+    '@nuxt/content'
+  ],
   devtools: { enabled: true },
   compatibilityDate: '2024-11-12',
   nuxtContentFromApi: {
-    apiBase: 'https://mocki.io/v1',
-    apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844',
-    fetchOption: {
-      method: null,
-      body: null,
-      headers: null,
-    },
-    slugAttribute: 'slug',
-    contentAttribute: 'content',
-    directory: './content/',
-    fileExtension: '.md',
-    verbose: true,
+    connections: [
+      {
+        apiBase: 'https://mocki.io/v1', // 'https://YOURHOST/api',
+        apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844', // '/blog/all'
+        fetchOption: {},
+        slugAttribute: 'slug',
+        contentAttribute: 'content',
+        directory: './playground/content/blog/',
+        fileExtension: '.md',
+        verbose: true,
+      },
+      {
+        apiBase: 'https://mocki.io/v1',
+        apiPath: '/a630127a-8605-42be-a9a2-c595d504de73', // CSV sample
+        fetchOption: {},
+        slugAttribute: 'slug',
+        contentAttribute: 'content',
+        directory: './playground/content/news/',
+        fileExtension: '.csv',
+        verbose: true,
+      },
+    ],
+    enabled: true,
   },
 })
