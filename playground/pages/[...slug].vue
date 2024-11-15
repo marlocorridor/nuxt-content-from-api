f4d31ccddb5b6d8537ef2f01ae817a9e7b85435d
Marlo <corridor.marlo@gmail.com>
2024-11-15 15:43:25 +0800

    Update playground for sample
    * add missed untracked files

---
+++ playground/pages/[...slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        {{ doc.title }}
+        <ContentRenderer :value="doc">
+          <template #empty>
+            <!-- see https://content.nuxt.com/components/content-renderer -->
+            <!-- Other types will currently be passed to default slot via v-slot="{ data }" or be rendered inside <pre /> tag. -->
+            <p>
+              <span v-if="doc._type != 'md'">This is a <b><code>{{ doc._type }}</code></b> document</span>
+            </p>
+            <p>No content found.</p>
+          </template>
+        </ContentRenderer>
+      </template>
+      <template #not-found>
+        <h1>Document not found</h1>
+      </template>
+    </ContentDoc>
+  </main>
+</template>
---
+++ playground/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav>
+    <ContentNavigation v-slot="{ navigation }">
+      <ul>
+        <li v-for="link of navigation" :key="link._path">
+          <NuxtLink :to="link._path">{{ link.navTitle || link.title }}</NuxtLink>
+        </li>
+      </ul>
+    </ContentNavigation>
+  </nav>
+  <main>
+    <h1>Index page</h1>
+
+    <h2>News</h2>
+    <ContentList path="/news" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <NuxtLink :to="article._path">
+          <h3>{{ article.title }}</h3>
+        </NuxtLink>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+
+    <h2>Blogs</h2>
+    <ContentList path="/blog" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <NuxtLink :to="article._path">
+          <h3>{{ article.title }}</h3>
+        </NuxtLink>
+        <p>{{ article.description }}</p>
+      </div>
+    </ContentList>
+  </main>
+</template>
+
+<script lang="ts" setup>
+</script>
