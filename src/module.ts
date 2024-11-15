439d856d57b1791c73cd78e632c03293e23703a8
Marlo <corridor.marlo@gmail.com>
2024-11-15 15:25:05 +0800

    Implement handling of multiple connections

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,7 @@
 import { existsSync, mkdirSync, writeFileSync } from 'node:fs'
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
-// Module options TypeScript interface definition
-export interface ModuleOptions {
+type Connection = {
   apiBase: string
   apiPath: string
   fetchOption: object
@@ -13,6 +12,12 @@ export interface ModuleOptions {
   verbose: boolean
 }
 
+// Module options TypeScript interface definition
+export interface ModuleOptions {
+  connections: Connection[]
+  enabled: boolean
+}
+
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-content-from-api',
@@ -20,67 +25,94 @@ export default defineNuxtModule<ModuleOptions>({
   },
   // Default configuration options of the Nuxt module
   defaults: {
-    apiBase: 'https://mocki.io/v1', // 'https://YOURHOST/api',
-    apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844', // '/blog/all'
-    fetchOption: {},
-    slugAttribute: 'slug',
-    contentAttribute: 'content',
-    directory: './content/',
-    fileExtension: '.md',
-    verbose: false,
+    connections: [
+      {
+        apiBase: 'https://mocki.io/v1', // 'https://YOURHOST/api',
+        apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844', // '/blog/all'
+        fetchOption: {},
+        slugAttribute: 'slug',
+        contentAttribute: 'content',
+        directory: './content/blog',
+        fileExtension: '.md',
+        verbose: true,
+      },
+      {
+        apiBase: 'https://mocki.io/v1',
+        apiPath: '/a630127a-8605-42be-a9a2-c595d504de73', // CSV sample
+        fetchOption: {},
+        slugAttribute: 'slug',
+        contentAttribute: 'content',
+        directory: './content/news/',
+        fileExtension: '.csv',
+        verbose: true,
+      },
+    ],
+    enabled: true,
   },
   setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
-
+    
     nuxt.hook('build:before', () => {
-      const url = options.apiBase + options.apiPath
-      fetch(url, options.fetchOption)
-        .then(response => response.json())
-        .then((jsonData) => {
-          console.info('Processing Content from API: Starting...')
+      // if enabled, continue to define hook
+      if (!options.enabled) { return }
 
-          // Log response if verbose option is true
-          if (options.verbose) {
-            console.info('Logging Content from API response...')
-            console.log(jsonData)
-            console.info('End of Logging API response.')
-          }
+      // Process each connection setting
+      options.connections.forEach((connection: Connection, connectionId: number) => {
+        console.group()
+        
+        const url = connection.apiBase + connection.apiPath
+        fetch(url, connection.fetchOption)
+          .then(response => response.json())
+          .then((jsonData) => {
+            console.info(`Processing Content from Connection ${connectionId}: Starting...`)
 
-          // create directory if not exist
-          if (!existsSync(options.directory)) {
-            mkdirSync(options.directory, {
-              // supports nested directory
-              recursive: true,
-            })
-          }
-
-          // Define keys for attribute options
-          const slug = options.slugAttribute
-          const content = options.contentAttribute
-
-          // Define with variable attribute. See reference: https://dev.to/jagroop2001/how-to-choose-your-type-typescript-interfaces-vs-types-51hc
-          type Entry = {
-            [key: string]: string
-          }
-
-          // write the file
-          jsonData.forEach((entry: Entry, i: number) => {
-            // skip if required data is not available
-            if (!entry[slug] || !entry[content]) {
-              console.error(`Data #${i}: "${slug}" or "${content}" not found. Skipping...`)
-              return
+            // Log response if verbose option is true
+            if (connection.verbose) {
+              console.info('Logging Content from API response...')
+              console.log(jsonData)
+              console.info('End of Logging API response.')
             }
 
-            // write the file using entry.slug as name for routing
-            writeFileSync(
-              options.directory + entry.slug + options.fileExtension,
-              entry[content], { flag: 'w' },
-            )
+            // create directory if not exist
+            if (!existsSync(connection.directory)) {
+              mkdirSync(connection.directory, {
+                // supports nested directory
+                recursive: true,
+              })
+            }
+
+            // Define keys for attribute options
+            const slug = connection.slugAttribute
+            const content = connection.contentAttribute
+
+            // Define with variable attribute. See reference: https://dev.to/jagroop2001/how-to-choose-your-type-typescript-interfaces-vs-types-51hc
+            type Entry = {
+              [key: string]: string
+            }
+
+            // write the file
+            jsonData.forEach((entry: Entry, i: number) => {
+              // skip if required data is not available
+              if (!entry[slug] || !entry[content]) {
+                console.error(`Data #${i}: "${slug}" or "${content}" not found. Skipping...`)
+                return
+              }
+
+              // write the file using entry.slug as name for routing
+              writeFileSync(
+                connection.directory + entry.slug + connection.fileExtension,
+                entry[content], { flag: 'w' },
+              )
+            })
+
+            console.info('Processing Content from API: Completed!')
           })
 
-          console.info('Processing Content from API: Completed!')
-        })
+        console.groupEnd()
+      }) // end of connections.forEach
+      console.info('Done pulling contents from API!')
     })
+
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
   },
