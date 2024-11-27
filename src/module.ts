34229c19a5db81b7ef459b76e8329e37b44d9fae
Marlo <corridor.marlo@gmail.com>
2024-11-27 17:24:43 +0800

    Remove `connections` defaults on module
    move to test

--- src/module.ts
+++ src/module.ts
@@ -25,28 +25,7 @@ export default defineNuxtModule<ModuleOptions>({
   },
   // Default configuration options of the Nuxt module
   defaults: {
-    connections: [
-      {
-        apiBase: 'https://mocki.io/v1', // 'https://YOURHOST/api',
-        apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844', // '/blog/all'
-        fetchOption: {},
-        slugAttribute: 'slug',
-        contentAttribute: 'content',
-        directory: './content/blog',
-        fileExtension: '.md',
-        verbose: true,
-      },
-      {
-        apiBase: 'https://mocki.io/v1',
-        apiPath: '/a630127a-8605-42be-a9a2-c595d504de73', // CSV sample
-        fetchOption: {},
-        slugAttribute: 'slug',
-        contentAttribute: 'content',
-        directory: './content/news/',
-        fileExtension: '.csv',
-        verbose: true,
-      },
-    ],
+    connections: [],
     enabled: true,
   },
   setup(options, nuxt) {
--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -5,14 +5,27 @@ export default defineNuxtConfig({
     MyModule,
   ],
   nuxtContentFromApi: {
-    apiBase: 'https://mocki.io/v1',
-    apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844',
-    fetchOption: {
-      method: null,
-      body: null,
-      headers: null,
-    },
-    directory: './content/',
-    verbose: true,
+    connections: [
+      {
+        apiBase: 'https://mocki.io/v1', // 'https://YOURHOST/api',
+        apiPath: '/5902631b-810f-4696-a6f6-0f89b7f99844', // '/blog/all'
+        fetchOption: {},
+        slugAttribute: 'slug',
+        contentAttribute: 'content',
+        directory: './content/blog/',
+        fileExtension: '.md',
+        verbose: true,
+      },
+      {
+        apiBase: 'https://mocki.io/v1',
+        apiPath: '/a630127a-8605-42be-a9a2-c595d504de73', // CSV sample
+        fetchOption: {},
+        slugAttribute: 'slug',
+        contentAttribute: 'content',
+        directory: './content/news/',
+        fileExtension: '.csv',
+        verbose: true,
+      },
+    ]
   },
 })
